7513085a744e32d855e78831204b8bdd9c430da6
Bryan Fonseca <isaac1805@hotmail.com>
2023-07-07 19:23:48 -0500

    First state management option

--- src/components/TouchTyper.js
+++ src/components/TouchTyper.js
@@ -1,19 +1,19 @@
+import { useState } from "react";
 import ResetButton from "./ResetButton";
 import Timer from "./Timer";
 import TyperInput from "./TyperInput";
 import WordsContainer from "./WordsContainer";
 
 function TouchTyper() {
+    const [typed, setTyped] = useState("");
     return (
         <div className="touch-typer">
             <div className="touch-typer__top">
                 <Timer />
-                <WordsContainer>
-                    {['hola ', 'como ', 'estas']}
-                </WordsContainer>
+                <WordsContainer>{["hola ", "como ", "estas"]}</WordsContainer>
             </div>
             <div className="touch-typer__bottom">
-                <TyperInput />
+                <TyperInput value={typed} onType={setTyped} />
                 <ResetButton />
             </div>
         </div>
--- src/components/TyperInput.js
+++ src/components/TyperInput.js
@@ -1,10 +1,12 @@
-function TyperInput() {
+function TyperInput({ value, onType }) {
     return (
         <input
             className="touch-typer__input"
             type="text"
             placeholder="type"
             autofocus
+            value={value}
+            onChange={(e) => onType(e.target.value)}
         />
     );
 }
