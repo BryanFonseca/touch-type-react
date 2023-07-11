4add24d19a029cd64355eaaf0836b7a211e0fc2b
Bryan Fonseca <isaac1805@hotmail.com>
2023-07-10 22:52:46 -0500

    Add basic auto scrolling logic

--- src/components/TouchTyper.js
+++ src/components/TouchTyper.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ResetButton from "./ResetButton";
 import Timer from "./Timer";
 import TyperInput from "./TyperInput";
@@ -15,21 +15,62 @@ const words = [
     "bien",
     "de",
     "nada",
+    "hola",
+    "como",
+    "estas",
+    "estas",
+    "yo",
+    "estoy",
+    "muy",
+    "bien",
+    "de",
+    "nada",
 ];
+
+function isInsideFirstLineArea(container, element) {
+    const { top: elementTop } = element.getBoundingClientRect();
+    const wordHeight = Number.parseFloat(
+        window.getComputedStyle(element).height
+    );
+    const { top: areaTop } = container.getBoundingClientRect();
+    const areaBottom = areaTop + wordHeight;
+    return elementTop <= areaBottom;
+}
 
 function TouchTyper() {
     const [currentWordIndex, setCurrentWordIndex] = useState(0);
     const [typingDetails, setTypingDetails] = useState({});
+    const wordsContainerRef = useRef();
+    const currentWordRef = useRef();
 
     function handleNextWord() {
         setCurrentWordIndex((prev) => prev + 1);
     }
 
+    useEffect(() => {
+        const { current: currentWordElement } = currentWordRef;
+        const { current: wordsContainerElement } = wordsContainerRef;
+        if (!currentWordElement) return;
+        if (!isInsideFirstLineArea(wordsContainerElement, currentWordElement)) {
+            const wordHeight = Number.parseFloat(
+                getComputedStyle(currentWordElement).height
+            );
+            const wordMarginTop = Number.parseFloat(
+                getComputedStyle(currentWordElement).marginTop
+            );
+            wordsContainerElement.scrollBy({
+                top: wordHeight + wordMarginTop * 2,
+                behavior: "smooth",
+            });
+        }
+    }, [currentWordIndex]);
+
     const wordsElements = words.map((word, wordIdx) => (
         <span
+            ref={wordIdx === currentWordIndex ? currentWordRef : null}
             className={`word ${
                 wordIdx === currentWordIndex ? "word--current" : ""
-            }`}
+            } ${wordIdx < currentWordIndex ? "word--passed" : ""}`}
             key={word + wordIdx}
         >
             {[...word].map((character, characterIdx) => (
@@ -58,7 +99,9 @@ function TouchTyper() {
         <div className="touch-typer">
             <div className="touch-typer__top">
                 <Timer />
-                <WordsContainer>{wordsElements}</WordsContainer>
+                <WordsContainer ref={wordsContainerRef}>
+                    {wordsElements}
+                </WordsContainer>
             </div>
             <div className="touch-typer__bottom">
                 <TyperInput
--- src/components/WordsContainer.js
+++ src/components/WordsContainer.js
@@ -1,9 +1,11 @@
-function WordsContainer({ children }) {
+import { forwardRef } from "react";
+
+const WordsContainer = forwardRef(function ({ children }, ref) {
     return (
         <div className="touch-typer__text-container">
-            <div className="touch-typer__text">{children}</div>
+            <div ref={ref} className="touch-typer__text">{children}</div>
         </div>
     );
-}
+});
 
 export default WordsContainer;
